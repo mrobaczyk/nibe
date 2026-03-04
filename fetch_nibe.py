45946eb5c0e8e4e9bc20377e12a7c9ecf7fb7506
mrobaczyk <mrobaczyk@man.poznan.pl>
2026-03-04 10:11:42 +0100

    More params.

--- fetch_nibe.py
+++ fetch_nibe.py
@@ -3,9 +3,25 @@ import json
 import os
 import time
 
-# Dane z Secrets
 CLIENT_ID = os.getenv('NIBE_CLIENT_ID')
 CLIENT_SECRET = os.getenv('NIBE_CLIENT_SECRET')
+
+# Mapa parametrów (ID: Nazwa w JSON)
+PARAMS_MAP = {
+    "40004": "outdoor",       # BT1
+    "40067": "outdoor_avg",   # BT1 śr.
+    "43009": "calc_flow",     # Obliczona
+    "40033": "flow",          # BT2
+    "40015": "liquid_line",   # BT17
+    "40013": "cwu_upper",     # BT7 (Góra)
+    "40014": "cwu_load",      # BT6 (Ładowanie)
+    "40032": "room_temp",     # BT50
+    "43420": "compressor_hz", # Hz
+    "43005": "degree_minutes",# GM
+    "43437": "pump_speed",    # GP1 (%)
+    "43416": "starts_total",  # Licznik uruchomień
+    "43414": "hours_total"    # Licznik godzin
+}
 
 def get_token():
     url = "https://api.myuplink.com/oauth/token"
@@ -22,23 +38,21 @@ def fetch_data():
     token = get_token()
     headers = {'Authorization': f'Bearer {token}'}
     
-    # 1. Pobierz systemy
     systems = requests.get("https://api.myuplink.com/v2/systems/me", headers=headers).json()
     sys_id = systems['systems'][0]['systemId']
     dev_id = systems['systems'][0]['devices'][0]['deviceId']
     
-    # 2. Pobierz parametry (Outdoor: 40004, Flow: 40033)
-    params_url = f"https://api.myuplink.com/v2/devices/{dev_id}/points?parameters=40004,40033"
+    ids_str = ",".join(PARAMS_MAP.keys())
+    params_url = f"https://api.myuplink.com/v2/devices/{dev_id}/points?parameters={ids_str}"
     points = requests.get(params_url, headers=headers).json()
     
-    # Przygotuj nowy wpis
-    new_entry = {
-        "timestamp": time.strftime("%Y-%m-%d %H:%M"),
-        "outdoor": next(p['value'] for p in points if p['parameterId'] == '40004'),
-        "flow": next(p['value'] for p in points if p['parameterId'] == '40033')
-    }
+    new_entry = {"timestamp": time.strftime("%Y-%m-%d %H:%M")}
     
-    # Wczytaj starą historię i dodaj nowy wpis
+    for p in points:
+        key = PARAMS_MAP.get(str(p['parameterId']))
+        if key:
+            new_entry[key] = p['value']
+    
     try:
         with open('data.json', 'r') as f:
             history = json.load(f)
@@ -46,8 +60,7 @@ def fetch_data():
         history = []
         
     history.append(new_entry)
-    # Trzymaj tylko ostatnie 100 pomiarów
-    history = history[-100:]
+    history = history[-3000:] # Historia na ok. 2 miesiące
     
     with open('data.json', 'w') as f:
         json.dump(history, f, indent=4)
